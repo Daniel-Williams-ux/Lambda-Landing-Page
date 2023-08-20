56fde6f9fb1fb45965c8de45ed6ac14649187044
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-08-20 09:00:42 +0000

    added amazon config file

---
+++ js/config.js
@@ -0,0 +1,10 @@
+windows._config = {
+  cognito: {
+    userPoolId: 'us-east-1_FvvKk1hnK',
+    userPoolClientId: '5sj4893c12ocp68r8o0q01glr6',
+    region: 'us-east-1'
+  },
+  api: {
+    invokeUrl: ""
+  }
+}
